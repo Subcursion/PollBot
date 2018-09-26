0168ae0084ef543543f9243899733240b7b1c2ac
Adam Beck <adam.beck95@live.com>
2018-09-25 21:00:41 -0400

    EOD commit

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var Poll = require("./poll.js");
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -23,57 +24,160 @@ bot.on('ready', function(evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+setInterval(function() {
+	// Clean up PollList for expired timers
+	if (PollList.length == 0)
+		return;
+	
+	logger.info("Cleaning up PollList (" + PollList.length + ")...");
+	for (i = 0; i < PollList.length; i++) {
+		if (PollList[i].isDone) {
+			PollList.splice(i, 1);
+			i--;
+		}
+	}
+	logger.info("All done. PollList (" + PollList.length + ")");
+}, 30000);
+
+var showNewPollUsage = function(channelID) {
+	bot.sendMessage({ to: channelID, message: "You seem confused. Here's how to do it: newpoll{poll: <poll text> time: #(s/m/h) option: <option text> option: <option text>} (Order doesn't matter, min 2 options needed, max of 6)"});
+}
 
 bot.on('message', function(user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch (cmd) {
-            // !ping
-            case 'poll':
-                var poll = new Poll("This is a test poll", 5, function() {
-					bot.sendMessage({
-						to: channelID,
-						message: "The poll has ended!"
-					});
-				});
-                var timer = setInterval(function() {
-						poll.incrementTime(-1);
-				}, 1000);
-				poll.setTimer(timer);
-                bot.sendMessage({
-                    to: channelID,
-                    message: "Permament poll system implemented. Hello from the Pi! Oh yeah the poll\n" + poll.poll + "\n" + poll.getTime()
-                });
-                
-                
-                break;
-        }
-    }
+	if (userID == bot.id)
+		return;
+		
+	if (message.includes("newpoll{")) {
+		// NEW POLL MODE
+		// seperate poll section out
+		var newpollLoc = message.indexOf("newpoll{");
+		// find closing brack '}'
+		var lastBrackLoc = message.indexOf("}", newpollLoc + 8);
+		if (lastBrackLoc == -1) {
+			bot.sendMessage({
+					to: channelID,
+					message: "Can't creat a new poll. Reason: Missing a closing bracket.. I know it's silly."
+			});
+			return;
+		}
+		// extract the string
+		var request = message.substring(newpollLoc + 8, lastBrackLoc);
+		var specialLocations = []
+		// check if everything we need is found
+		// find poll: location
+		var pollLoc = request.indexOf("poll:");
+		if (pollLoc == -1) {
+			showNewPollUsage(channelID);
+			return;
+		}
+		specialLocations.push(pollLoc);
+		// find time: location
+		var timeLoc = request.indexOf("time:");
+		if (timeLoc == -1) {
+			showNewPollUsage(channelID);
+			return;
+		}
+		specialLocations.push(timeLoc);
+		// then find the options
+		var optionLoc = request.indexOf("option:");
+		// check if none were even found
+		if (optionLoc == -1) {
+			showNewPollUsage(channelID);
+			return;
+		}
+		// otherwise load their locations in
+		var optionLocs = [];
+		while (optionLoc != -1) {
+			specialLocations.push(optionLoc);
+			optionLocs.push(optionLoc);
+			optionLoc = request.indexOf("option:", optionLoc + 1);
+		}
+		if (optionLocs.length < 2) {
+			showNewPollUsage(channelID);
+			return;
+		} else if (optionLocs.length > 6) {
+			showNewPollUsage(channelID);
+			return;
+		}
+		
+		// for each extraction, go until another special index is found, substring to there, there ya go!
+		// extract poll: string
+		var index = pollLoc + 5;
+		while (index < request.length && !(specialLocations.includes(index))) {
+			index++;
+		}
+		var pollText = request.substring(pollLoc + 5, index).trim();
+		if (pollText.length == 0) {
+			bot.sendMessage({ to: channelID, message: "Your poll was empty!" });
+			return;
+		}
+		
+		// extract time: string
+		index = timeLoc + 5;
+		while (index < request.length && !(specialLocations.includes(index))) {
+			index++;
+		}
+		var timeText = request.substring(timeLoc + 5, index).trim();
+		if (timeText.length == 0) {
+			bot.sendMessage({ to: channelID, message: "Your time was empty!" });
+			return;
+		}
+		
+		// extract all option: strings
+		var optionStrings = []
+		for (i = 0; i < optionLocs.length; i++) {
+			index = optionLocs[i] + 7;
+			while (index < request.length && !(specialLocations.includes(index))) {
+				index++;
+			}
+			var optionText = request.substring(optionLocs[i] + 7, index).trim();
+			if (optionText.length == 0) {
+				bot.sendMessage({ to: channelID, message: "One of your options was empty!" });
+				return;
+			}
+			optionStrings.push(optionText);
+		}
+		
+		// convert timeText to seconds
+		// check for s/m/h at end
+		var unit = timeText.substring(timeText.length - 1, timeText.length);
+		if (unit != "s" && unit != "m" && unit != "h" && unit != "d") {
+			bot.sendMessage({ to: channelID, message: "Unrecognized time unit used. Please use s (seconds), m (minutes), h (hours), or d (days) like so time: 60s" });
+			return;
+		}
+		// strip the unit out
+		var time = parseInt(timeText);
+		if (isNaN(time)) {
+			bot.sendMessage({ to: channelID, message: "I couldn't read your time. Please enter it as #s/m/h/d like so time: 60s" });
+			return;
+		}
+		if (time <= 0) {
+			bot.sendMessage({ to: channelID, message: "Kind of pointless to have a 0 or negative time isn't it?" });
+			return;
+		}
+		// convert to seconds
+		if (unit == "m")
+			time *= 60;
+		if (unit == "h")
+			time *= 60 * 60;
+		if (unit == "d")
+			time *= 60 * 60 * 24;
+		
+		// by this point, we should be ready to actually create the new poll!
+		var poll = new Poll(channelID, pollText, time, optionStrings, function(message) {
+			bot.sendMessage(poll.broadcast(message));
+		});
+		
+		var timer = setInterval(function() {poll.incrementTime(-1);}, 1000);
+		poll.setTimer(timer);
+		PollList.push(poll);
+		
+		bot.sendMessage(poll.broadcast("The poll has started! Get your votes in!"));
+		
+	} else if (message.includes("viewpoll{")) {
+		
+	} else if (message.includes("votepoll{")) {
+		
+	}
 });
 
-function Poll(poll, timer, callback) {
-    this.poll = poll;
-    this.timer = timer;
-    this.callback = callback;
-    
-    this.timerObj = null;
-    
-    this.incrementTime = function(timerAmt) {
-		this.timer += timerAmt;
-		if (this.timer <= 0) {
-			this.callback();
-			clearInterval(this.timerObj);
-		}
-	};
-	
-	this.setTimer = function(timerObj) {
-		this.timerObj = timerObj;
-	};
-    
-    this.getTime = function() {
-        return "Time left is " + this.timer + " seconds";
-    };
-}
--- poll.js
+++ poll.js
@@ -1,23 +1,29 @@
 class Poll {
 
-	constructor(channelID, pollText, time, options, callback) {
+	constructor(channelID, pollText, time, options, sendMessage) {
 		this.pollText = pollText;
 		this.time = time;
 		this.options = options;
-		this.callback = callback;
+		this.sendMessage = sendMessage;
 		this.timerObj = null;
 		this.isDone = false;
 		this.channelID = channelID;
 		// make random color
 		this.color = parseInt("0x" + (Math.random().toString(16) + "000000").slice(2, 8));
+		this.reminderTimes = [60, 60 * 5, 60 * 30, 60 * 60, 60 * 60 *6, 60 * 60 * 12, 60 * 60 * 24];	
+		
 	}	
 	
 	incrementTime(timerAmt) {
 		this.time += timerAmt;
 		
+		if (this.reminderTimes.includes(this.time)) {
+			this.sendMessage("Poll is ending soon, get your votes in fast!");
+		}
+		
 		if (this.time <= 0) {
 			this.isDone = true;
-			this.callback();
+			this.sendMessage("The poll has finished!");
 			clearInterval(this.timerObj);
 		}
 	}
@@ -30,9 +36,9 @@ class Poll {
 		// make procedural options array
 		var footerObj = {}
 		var fields = [];
-		var letters = ["A", "B", "C", "D"];
+		var letters = ["A", "B", "C", "D", "E", "F"];
 		for (i = 0; i < this.options.length; i++) {
-			fields.push({"name": letters[i] + ") " + this.options[i], "value": "20% (4 users)", "inline": true});
+			fields.push({"name": letters[i] + ") " + this.options[i], "value": "20% (4 users)", "inline": true });
 		}
 		if (!this.isDone) {
 			// Code for adjust to poll being active
@@ -41,12 +47,26 @@ class Poll {
 				"value": "Use votepoll{poll:" + this.pollText.split(" ").slice(0, 2).join(" ") + " option: " + 
 						letters.slice(0, this.options.length).join("/") + "} to vote!"			
 			});
-			footerObj.text = "Time left: " + this.time + " seconds";
+			// rework footer text for time left to show 1 minute, 2 hours, etc
+			// first fine biggest time measurement that fits (seconds, minutes, hours, days)
+			if (this.time >= (60 * 60 * 24)) {
+				var daycount = this.time / (60 * 60 * 24);
+				footerObj.text = "Time left: " + daycount + " day" + (daycount > 1 ? "s" : "");
+			} else if (this.time >= (60 * 60)) {
+				var hours = this.time / (60 * 60);
+				footerObj.text = "Time left: " + hours + " hour" + (hours > 1 ? "s" : "");
+			} else if (this.time >= 60) {
+				var minutes = this.time / 60;
+				footerObj.text = "Time left: " + minutes + " minute" + (minutes > 1 ? "s" : "");
+			} else {
+				footerObj.text = "Time left: " + this.time + " second" + (this.time > 1 ? "s" : "");
+			}
 			footerObj.icon_url = "https://upload.wikimedia.org/wikipedia/commons/3/37/Clock.gif"
 		} else {
 			// Code to adjust for the poll being done
 			footerObj.text = "Poll finished";
 		}
+		
 		return {
 			"to": this.channelID,
 			"embed": {
